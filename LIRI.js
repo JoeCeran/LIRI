c542d5ebba17a48cb0e3998fa40dec43a6aa16ce
Joseph Cerankowski <joecerankowski@gmail.com>
2019-01-29 14:45:43 -0500

    yes

--- LIRI.js
+++ LIRI.js
@@ -1,14 +1,15 @@
+require("dotenv").config();
 
-var NodeGeocoder = require("node-geocoder");
 var inquirer = require("inquirer");
+var axios = require("axios");
+var moment = require("moment");
+var fs = require("fs");
+var keys = require("./keys.js");
+var Spotify = require('node-spotify-api');
+var spotify = new Spotify(keys.spotify);
+var artist;
 
-// Replace with your mapquest consumer API key
-var options = {
-  provider: "mapquest",
-  apiKey: "YOUR-MAPQUEST-API-CONSUMER-KEY"
-};
 
-var geocoder = NodeGeocoder(options);
 
 
 // Prompt the user to provide location information.
@@ -17,22 +18,170 @@ inquirer.prompt([
   {
     type: "list",
     name: "command",
-    message: ["Look up band events", "Look up song info", "Look up movie info", "Do what it says"]
+    message: "What would you like to do?",
+    choices: ["Look up band events", "Look up song info", "Look up movie info", "Do what it says"]
   }
 
-// After the prompt, store the user's response in a variable called location.
+// After the prompt, decideds which choice was chosen
 ]).then(function(user) {
 
     if (user.command == "Look up band events") {
-
-    }
-    else if (user.command == "Look up song info") {
-
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "userInput",
+                message: "What band's events would you like to look up?"
+            } 
+            ]).then(function (user) {
+            concertThis(user.userInput);
+            })
+    } else if (user.command == "Look up song info") {
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "userInput",
+                message: "What song would you like to look up?",
+            }
+            ]).then(function (user) {
+            spotifyThis(user.userInput);
+            })
     }
     else if (user.command == "Look up movie info") {
-
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "userInput",
+                message: "What movie would you like to look up?",
+            }
+            ]).then(function(user) {  
+            movieThis(user.userInput);
+            })
     }
     else if(user.command == "Do what it says") {
-
+    doWhatItSays();
     }
 });
+
+// Runs the bands in town API
+    function concertThis(artist) {
+        if (artist != ""){     
+        let queryUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+        axios.get(queryUrl).then(function (response) {
+           
+                for (let i = 0; i < 5; i++) {
+                    console.log ("Venue: " +  response.data[i].venue.name);
+                    console.log ("Location: " +  response.data[i].venue.city);
+                    console.log ("Date: " +  moment(response.data[i].datetime).format("MM/DD/YYYY"));
+                    console.log (" ");
+                }
+              })
+            }
+        else {
+            console.log("Cher");
+            let queryUrl = "https://rest.bandsintown.com/artists/Cher/events?app_id=codingbootcamp";
+            axios.get(queryUrl).then(function (response) {
+            
+                    for (let i = 0; i < 5; i++) {
+                        console.log ("Venue: " +  response.data[i].venue.name);
+                        console.log ("Location: " +  response.data[i].venue.city);
+                        console.log ("Date: " +  moment(response.data[i].datetime).format("MM/DD/YYYY"));
+                        console.log (" ");
+                    }
+                })
+            }
+        };
+    
+    // Runs the Spotify API
+    function spotifyThis(song) {       
+                // We then run the request with axios module on a URL with a JSON
+                if (song != ""){
+                spotify
+                .search({ type: 'track', query: song })
+                .then(function(response) {
+                    for (let i = 0; i < 5; i++){
+                    console.log("Artists: " + response.tracks.items[i].artists[0].name);
+                    console.log("Song name: " + response.tracks.items[i].name);
+                    console.log("Preview Link: " + response.tracks.items[i].preview_url);
+                    console.log("Album: " + response.tracks.items[i].album.name);
+                    }
+                })
+            }
+            else {
+                console.log("The Sign, Ace of Base");
+            spotify
+                .search({ type: 'track', query: "the sign ace of base" })
+                .then(function(response) {
+                    
+                    console.log("Artists: " + response.tracks.items[0].artists[0].name);
+                    console.log("Song name: " + response.tracks.items[0].name);
+                    console.log("Preview Link: " + response.tracks.items[0].preview_url);
+                    console.log("Album: " + response.tracks.items[0].album.name);
+                    
+                })
+            }
+        };
+    //Runs the OMBD API
+    function movieThis(movie) {
+    
+            if (movie != ""){ 
+            // We then run the request with axios module on a URL with a JSON
+            axios.get("http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy").then(function(response) {
+                // Then we print out the imdbRating
+                console.log("Movie title: " + response.data.Title);
+                console.log("Year: " + response.data.Year);
+                console.log("imdb rating: " + response.data.imdbRating);
+                console.log("Rotten Tomatos rating: " + response.data.Ratings[1].Value);
+                console.log("Country: " + response.data.Country);
+                console.log("Language: " + response.data.Language);
+                console.log("Plot: " + response.data.Plot);
+                console.log("Actors: " + response.data.Actors);
+            })
+            }
+            else {
+                axios.get("http://www.omdbapi.com/?t=Mr.Nobody&y=&plot=short&apikey=trilogy").then(function(response) {
+                    // Then we print out the imdbRating
+                    console.log("Have you seen Mr.Nobody?");
+                    console.log("It's on Netflix!");
+                    console.log("Movie title: " + response.data.Title);
+                    console.log("Year: " + response.data.Year);
+                    console.log("imdb rating: " + response.data.imdbRating);
+                    console.log("Rotten Tomatos rating: " + response.data.Ratings[1].Value);
+                    console.log("Country: " + response.data.Country);
+                    console.log("Language: " + response.data.Language);
+                    console.log("Plot: " + response.data.Plot);
+                    console.log("Actors: " + response.data.Actors);
+                })
+            }
+        }
+    // Runs the do what it says command
+    var doWhatItSays = function() {
+    fs.readFile("random.txt", "utf8", function(error, data) {
+
+        // If the code experiences any errors it will log the error to the console.
+        if (error) {
+          return console.log(error);
+        }
+      
+        // We will then print the contents of data
+        console.log(data);
+      
+        // Then split it by commas (to make it more readable)
+        var dataArr = data.split(",");
+      
+        // We will then re-display the content as an array for later use.
+        console.log(dataArr);
+        var randomNum = Math.floor(Math.random() * 3);
+            if (randomNum == 0) {
+            concertThis(dataArr);
+            }
+            else if (randomNum == 1){
+            spotifyThis(dataArr);   
+            }
+            else if (randomNum == 2){
+            movieThis(dataArr);
+            }
+            else{
+                console.log(randomNum);
+            }
+      });          
+    }
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # LIRI
+
+What is this project?
+
+I have created the liri program using NODE.js to look up information about artists events, music tracks, and movies using the OMBD, Spotify, and Bands in town APIS.
+
+Why is this project useful?
+
+It has allowed me to work more with APIs and creating programs using node.
+
+Who wrote this program?
+
+Joe Cerankowski
